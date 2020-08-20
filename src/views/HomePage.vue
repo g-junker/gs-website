917ddc028ced06bcad0003a78536c604ed80ed32
Glenda Junker <glendajunker@hotmail.com>
2020-08-20 18:51:51 +0200

    testing meta:og

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -54,6 +54,10 @@ export default {
         name: "description",
         content:
           "A Gonzaga Sampaio iniciou suas atividades em 2011, com a proposta de desenvolver projetos diferenciados em Santa Catarina, independentemente do padrão de cada empreendimento, focando sempre no conforto dos clientes e no potencial de valorização dos imóveis entregues."
+      },
+      {
+        property: "og:image",
+        content: require("@/assets/images/home-terroir.jpg")
       }
     ]
   }
